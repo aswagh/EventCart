340d1fa91035217edeb95a94de327bde59be1309
atul <aswagh303@gmail.com>
2023-08-12 14:17:51 +0530

    Pushing changes manded on order service - added circuit breaker

--- OrderService/src/main/java/com/aswagh/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/aswagh/OrderService/external/client/PaymentService.java
@@ -1,15 +1,26 @@
 package com.aswagh.OrderService.external.client;
 
+import com.aswagh.OrderService.exceptions.CustomException;
 import com.aswagh.OrderService.external.request.PaymentReqeust;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker( name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping("/doPayment")
     public ResponseEntity<Long> doPayment(@RequestBody PaymentReqeust paymentReqeust);
 
+    default void fallback(Exception e){
+        throw new CustomException(
+                "Payment service is currenty down",
+                "PAYMENT_SERVICE_DOWN",
+                500);
     }
+}
+
+
--- OrderService/src/main/java/com/aswagh/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/aswagh/OrderService/external/client/ProductService.java
@@ -1,14 +1,24 @@
 package com.aswagh.OrderService.external.client;
 
+import com.aswagh.OrderService.exceptions.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
     @PutMapping("/reduceQuantity/{productId}")
     public ResponseEntity<Void> reduceQuantity(@PathVariable Long productId, @RequestParam Long quantity);
+
+    default void fallbakc(Exception e){
+        throw new CustomException(
+        "Product service is currenty down",
+                "PRODUCT_SERVICE_DOWN",
+                500);
+    }
 }
